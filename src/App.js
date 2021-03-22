40f293a3ce65dca07578dc2d1b000a7d745323a4
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-03-22 23:10:04 +0300

    Выносит селектор и очки в отдельные компоненты

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import React from 'react';
 import './App.css';
-import { categories } from './categories';
+import CategorySelector from './components/CategorySelector';
+import Scoreboard from './components/Scoreboard';
 
 function App() {
 	return (
 		<div className='app'>
 			<div className='question-header'>
-				<div className='category-selector'>
-					<p>Select Category</p>
-					<select>
-						{categories.map((category, index) => (
-							<option key={index} value={category.id}>
-								{category.name}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className='scoreboard'>
-					<div className='wrong'>
-						<strong>0</strong>
-						<span>wrong</span>
-					</div>
-					<div className='correct'>
-						<strong>0</strong>
-						<span>correct</span>
-					</div>
-				</div>
+				<CategorySelector />
+				<Scoreboard />
 			</div>
 		</div>
 	);
