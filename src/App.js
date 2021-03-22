09452fea7550e05e0e572c02a1472870862cddc4
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-03-22 23:07:56 +0300

    Добавляет хедер для игры

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import './App.css';
+import { categories } from './categories';
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
+		<div className='app'>
+			<div className='question-header'>
+				<div className='category-selector'>
+					<p>Select Category</p>
+					<select>
+						{categories.map((category, index) => (
+							<option key={index} value={category.id}>
+								{category.name}
+							</option>
+						))}
+					</select>
+				</div>
+				<div className='scoreboard'>
+					<div className='wrong'>
+						<strong>0</strong>
+						<span>wrong</span>
+					</div>
+					<div className='correct'>
+						<strong>0</strong>
+						<span>correct</span>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
---
+++ src/categories.js
@@ -0,0 +1,8 @@
+export const categories = [
+	{
+		id: 'any',
+		name: 'Any Category',
+	},
+	{ id: 9, name: 'General Knowledge' },
+	{ id: 10, name: 'Entertainment: Books' },
+];
